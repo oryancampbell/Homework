03e500d0763fefadd2efa0eecdfde3f3cdb58823
oryancampbell <55443262+oryancampbell@users.noreply.github.com>
2019-09-19 08:37:19 -0400

    Add files via upload

---
+++ 2019-09-19-Homework/2019-09-19-Homework/Main.cpp
@@ -0,0 +1,22 @@
+// Name: O'Ryan Campbell
+// Intro to Computer Science
+// Period 1
+// Mr. Williams
+// 2019-09-19-Homework
+
+#include <iostream>
+int main()
+{
+	std::cout << "Name: O'Ryan Campbell" << std::endl
+		<< "Class Schedule" << std::endl
+		<< "Period 1:Computer Science" << std::endl
+		<< "Period 2:House" << std::endl
+		<< "Period 3:Earth Science Lab" << std::endl
+		<< "Period 4:Health" << std::endl
+		<< "Period 5:Geometry" << std::endl
+		<< "Period 6:Spanish 1" << std::endl
+		<< "Period 7:Lunch" << std::endl
+		<< "Period 8:Global History" << std::endl
+		<< "Period 9:Earth Science" << std::endl
+		<< "Period 10:English 10" << std::endl;
+}
