2cffb055b09c70081fcebf6ad434a079112a8e97
oryancampbell <55443262+oryancampbell@users.noreply.github.com>
2019-10-04 08:43:31 -0400

    Add files via upload

---
+++ 2019-10-04-Homework/2019-10-04-Homework/Main.cpp
@@ -0,0 +1,19 @@
+// Name: O'Ryan Campbell
+// Intro to Computer Science
+// Mr. Williams 
+// Period 1
+// 2019-10-04-Homework
+
+#include <istream>
+#include <iostream>
+using namespace std;
+int main()
+{
+	float r = 5.5;
+	float pi = 3.14;
+	float p = 2 * r * pi;
+	float a = r * r * pi;
+	cout << "Area =";
+
+
+}
